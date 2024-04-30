7bbd35bfee1fd05d3b00cf126522cc207bb118bb
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2024-04-30 18:02:21 +0300

    add shadcn with dark mode and create header

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import { ModeToggle } from "@/components/mode-toggle";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+
+const Header = () => {
+    return (
+        <header className="flex justify-between items-center px-[5vw] max-h-20 w-full border-b">
+            <div className="flex items-center cursor-pointer gap-x-3 hover:opacity-80">
+                <Image src={"/light-logo.png"} width={100} height={100} alt="Logo" className="block dark:hidden max-sm:w-20" />
+                <Image src={"/dark-logo.png"} width={100} height={100} alt="Logo" className="hidden dark:block" />
+                <span className="text-2xl font-black max-sm:hidden">Ampu Fitness</span>
+            </div>
+            <nav className="flex gap-x-5 max-md:gap-x-3">
+                <ModeToggle />
+                <Link href={"/login"} className={buttonVariants({ variant: "outline" })}>Login</Link>
+                <Link href={"/login"} className={buttonVariants({ variant: "secondary" })}>Dashboard</Link>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
