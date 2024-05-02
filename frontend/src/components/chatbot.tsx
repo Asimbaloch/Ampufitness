562c3ddb3944b1a6a8d5c2629e0e542eae4e362c
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2024-05-02 11:41:20 +0300

    small ui changes to chatbot

--- frontend/src/components/chatbot.tsx
+++ frontend/src/components/chatbot.tsx
@@ -1,7 +1,12 @@
+import { Input } from "./ui/input";
+
 const Chatbot = () => {
+    const messages = [];
+
     return (
-        <div>
+        <div className="flex flex-col items-center justify-between h-full mx-5">
             this is the chatbot
+            <Input placeholder="Enter what are you looking for" className="fixed bottom-0 max-w-4xl mb-16 max-lg:w-11/12" />
         </div>
     )
 }
